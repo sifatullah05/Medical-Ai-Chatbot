7844b4ea57140fd1ae71f0f8ad5c30a51e8026e8
MD SIFATULLAH <mdsifatullah292@gmail.com>
2026-01-21 13:53:31 +0600

    readme file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Ai Chatbot',
+    version= '0.0.0',
+    author= 'MD SIFATULLAH',
+    author_email= 'mdsifatullah292@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
